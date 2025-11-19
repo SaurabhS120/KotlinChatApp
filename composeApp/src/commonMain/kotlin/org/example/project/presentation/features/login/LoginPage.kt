b8d6885356a2f1902fdbcd634f27204900500311
Saurabh Sonar <saurabh.sonar120@gmail.com>
2025-11-19 18:13:34 +0530

    Login UI

--- composeApp/src/commonMain/kotlin/org/example/project/presentation/features/login/LoginPage.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/presentation/features/login/LoginPage.kt
@@ -1,13 +1,18 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,33 +20,48 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinchatapp.composeapp.generated.resources.Res
 import kotlinchatapp.composeapp.generated.resources.compose_multiplatform
 import org.example.project.Greeting
 import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
+@Preview
 @Composable
 fun LoginPage(){
-    var showContent by remember { mutableStateOf(false) }
+    var userName by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .background(MaterialTheme.colorScheme.primaryContainer)
             .safeContentPadding()
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Button(onClick = { showContent = !showContent }) {
-            Text("Click me!")
+        Text("Login Page",fontSize = 36.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(48.dp))
+        Column {
+            Text("Username")
+            TextField(
+                value = userName,
+                onValueChange = { newText -> userName = newText },
+                label = { Text("Enter text") }
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Text("Password")
+            TextField(
+                value = password,
+                onValueChange = { newText -> password = newText },
+                label = { Text("Enter text") }
+            )
         }
-        AnimatedVisibility(showContent) {
-            val greeting = remember { Greeting().greet() }
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Image(painterResource(Res.drawable.compose_multiplatform), null)
-                Text("Compose: $greeting")
-            }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {}, modifier = Modifier.padding(12.dp)){
+            Text("Login", modifier = Modifier.padding(horizontal = 12.dp))
         }
     }
 }
