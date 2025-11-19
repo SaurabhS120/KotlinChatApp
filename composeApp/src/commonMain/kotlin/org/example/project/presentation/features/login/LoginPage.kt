681d21c12f6d5a1d38684efb1ec2c989ae5b83bb
Saurabh Sonar <saurabh.sonar120@gmail.com>
2025-11-19 18:36:30 +0530

    Login snackbar

--- composeApp/src/commonMain/kotlin/org/example/project/presentation/features/login/LoginPage.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/presentation/features/login/LoginPage.kt
@@ -11,12 +11,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,6 +32,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinchatapp.composeapp.generated.resources.Res
 import kotlinchatapp.composeapp.generated.resources.compose_multiplatform
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import org.example.project.Greeting
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -35,40 +45,56 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 fun LoginPage(){
     val viewModel = viewModel { LoginViewModel() }
-    Column(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.primaryContainer)
-            .safeContentPadding()
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("Login Page",fontSize = 36.sp, fontWeight = FontWeight.Bold)
-        Spacer(modifier = Modifier.height(48.dp))
-        Column {
-            Text("Username")
-            TextField(
-                value = viewModel.userName.value,
-                onValueChange = { newText -> viewModel.userName.value = newText },
-                label = { Text("Enter text") }
-            )
-            Spacer(modifier = Modifier.height(12.dp))
-            Text("Password")
-            TextField(
-                value = viewModel.password.value,
-                onValueChange = { newText -> viewModel.password.value = newText },
-                label = { Text("Enter text") }
-            )
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    LaunchedEffect(Unit) {
+        viewModel.events.collect { event ->
+            scope.launch {
+                snackbarHostState.showSnackbar(event)
+            }
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {}, modifier = Modifier.padding(12.dp)){
-            Text("Login", modifier = Modifier.padding(horizontal = 12.dp))
+    }
+    Scaffold ( snackbarHost = { SnackbarHost(snackbarHostState) }){
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .safeContentPadding()
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Login Page",fontSize = 36.sp, fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.height(48.dp))
+            Column {
+                Text("Username")
+                TextField(
+                    value = viewModel.userName.value,
+                    onValueChange = { newText -> viewModel.userName.value = newText },
+                    label = { Text("Enter text") }
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                Text("Password")
+                TextField(
+                    value = viewModel.password.value,
+                    onValueChange = { newText -> viewModel.password.value = newText },
+                    label = { Text("Enter text") }
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {scope.launch { viewModel.login() }}, modifier = Modifier.padding(12.dp)){
+                Text("Login", modifier = Modifier.padding(horizontal = 12.dp))
+            }
+            Text("username : ${viewModel.userName.value}")
+            Text("password : ${viewModel.password.value}")
         }
-        Text("username : ${viewModel.userName.value}")
-        Text("password : ${viewModel.password.value}")
     }
 }
 class LoginViewModel : ViewModel() {
+    private val _events = MutableSharedFlow<String>()
+    val events = _events.asSharedFlow()
     var userName = mutableStateOf("")
     var password = mutableStateOf("")
+    suspend fun login(){
+        _events.emit("Login  - UserName : ${userName.value},Password : ${password.value}")
+    }
 }
